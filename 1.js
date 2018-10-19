e2962061f9f572ee0e8ddf48bf8a12001382d63d
Nurul Huda <37390979+nurulhuda859@users.noreply.github.com>
2018-10-19 10:07:46 +0600

    Simplyfied JS code

--- 1.js
+++ 1.js
@@ -1,7 +1,10 @@
+
+var aln = "Welcome! You are a member of AAIA"
+var alm = "You are not a member of AAIA"
 var name = prompt ("What's your name?");
-if (name==="akas" ||"Akas" ||"apon" ||"Apon" ||"emon" ||"Emon" ||"asik" ||"Asik"){
-alert("Welcome! You are a member of AAIA");
+if (name==="akas"||name==="apon"||name==="imon"||name==="asik"||name==="Akas"||name==="Apon"||name==="Imon"||name==="Asik"){
+alert(aln);
 }
 else{
-alert("You are not a member of AAIA");
+alert(alm);
 }
