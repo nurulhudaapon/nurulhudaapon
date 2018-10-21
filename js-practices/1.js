86e8c8590fac689956b69aec41f6326b62ddef76
Nurul Huda <37390979+nurulhuda859@users.noreply.github.com>
2018-10-21 10:44:48 +0600

    added more action list and designed home page

--- js-practices/1.js
+++ js-practices/1.js
@@ -1,15 +1,47 @@
+var js1 = document.getElementById("js-1");
+var js2 = document.getElementById("js-2");
+var js3 = document.getElementById("js-3");
+var js4 = document.getElementById("js-4");
+var js5 = document.getElementById("js-5");
+var js6 = document.getElementById("js-6");
 
-var aln = "Welcome! You are a member of AAIA";
-var alm = "You are not a member of AAIA";
-var button = document.getElementById("button1")
-
-function check(){
-	var name = prompt ("What's your name?");
-if (name==="akas"||name==="apon"||name==="imon"||name==="asik"||name==="Akas"||name==="Apon"||name==="Imon"||name==="Asik"){
-alert(aln);
+function nAvailable(){
+document.write(
+"<h1 style='text-align:center'>This JS Program is still under development. <br>Thanks for trying my js</h1>")
 }
-else{
-alert(alm);
-document.write("<h1 style='text-align:center'>Thanks for using our person validator</h1>")
-}}
-button.addEventListener("click", check)
+
+// Action for JS Practice
+function js1a(){
+function greeting(){document.write("<h1 style='text-align:center'>Thanks for using our person validator</h1>")}
+var name = prompt ("What's your name?");
+if (name==="akas"||name==="apon"||name==="imon"||name==="asik"||name==="Akas"||name==="Apon"||name==="Imon"||name==="Asik"){
+alert("Welcome! " +name+" You are a member of AAIA");
+greeting()
+}else{
+alert("Sorry " +name+" You are not a member of AAIA");
+greeting()}
+}
+function js2a(){
+nAvailable()
+}
+function js3a(){
+nAvailable()
+}
+function js4a(){
+nAvailable()
+}
+function js5a(){
+nAvailable()
+}
+function js6a(){
+nAvailable()
+}
+// Event Listener
+js1.addEventListener("click", js1a)
+js2.addEventListener("click", js2a)
+js3.addEventListener("click", js3a)
+js4.addEventListener("click", js4a)
+js5.addEventListener("click", js5a)
+js6.addEventListener("click", js6a)
+js7.addEventListener("click", js7a)
+
