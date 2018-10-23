22a29ba101ed8d3884d73a7f8529bebdb5a3190f
Nurul Huda <37390979+nurulhuda859@users.noreply.github.com>
2018-10-23 18:13:33 +0600

    Update js-practices/1.js

--- js-practices/1.js
+++ js-practices/1.js
@@ -21,14 +21,44 @@ greeting()
 alert("Sorry " +name+" You are not a member of AAIA");
 greeting()}
 }
+// Browser Crasher
 function js2a(){
-nAvailable()
+for (var i=1; i>0; i++){
+	console.log(i);
 }
+}
+// Login Form
 function js3a(){
-nAvailable()
+var user = prompt("Username:");
+var pass = prompt("Password:");
+if (user==="apon" & pass === "123456"){
+	alert("Thanks for Login");
+}else{alert("Wrong Username and Password");}
 }
+// Facebook 2
 function js4a(){
-nAvailable()
+	var userlist = [{username: "apon", password: "123456"}, {username: "akas", password: "12345"}, {username: "asik", password: "1234"}];
+
+function js4a(username, password){
+if (username===userlist[0].username & password === userlist[0].password){
+alert("Thanks for Login");}
+else {alert("Wrong Username and Password");}}
+
+var userp = prompt("Username:");
+var passp = prompt("Password:");
+js4a (userp, passp);
+// var userlist = [{Username: "apon", Password: "123456"}, {Username: "akas", Password: "12345"}, {Username: "asik", Password: "1234"}];
+// var user = prompt("Username:");
+// var pass = prompt("Password:");
+// 								function check(){
+// 								for (var i=0; i < userlist.length; i++){ 
+// 									if (user===userlist[i].Username & pass === userlist[i].Password){
+// 									return true;
+// 								} else{return false;}
+// 							}
+// 						}
+// 						console.log(check());
+
 }
 function js5a(){
 nAvailable()
@@ -43,5 +73,3 @@ js3.addEventListener("click", js3a)
 js4.addEventListener("click", js4a)
 js5.addEventListener("click", js5a)
 js6.addEventListener("click", js6a)
-js7.addEventListener("click", js7a)
-
