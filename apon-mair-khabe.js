2413a0de669992bb274375f569eeba0d3af4ff21
Nurul Huda <nurulhuda859g@gmail.com>
2019-02-01 17:08:10 +0600

    added loader

--- apon-mair-khabe.js
+++
@@ -1,3 +0,0 @@
-const killApon = () => {
-	promt('Slap Apon')
-};
---
+++ js/main.js
@@ -0,0 +1,17 @@
+const loaded = () => {
+    document.querySelector('.preloader').style.display = "none";
+};
+
+function fadeOutEffect() {
+    var fadeTarget = document.getElementById("target");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 70);
+}
