96ce5295ddc1d402f9ffc7bf313eaaac866b52ae
Nurul Huda (Apon) <me@nurulhudaapon.com>
2026-05-28 01:24:31 +0600

    refactor: upgrade to Zig 0.16.0

--- src/language.zig
+++ src/language.zig
@@ -2,20 +2,23 @@ const std = @import("std");
 const util = @import("util.zig");
 const print = std.debug.print;
 
-pub fn main() void {
+pub fn main(init: std.process.Init) void {
+
     // Assignment
     util.title("Assignments");
     const person = "Nurul";
+    const long_sentence: []const u8 = "Hi there";
+    print("{s}", .{long_sentence});
     var age: u8 = 23;
     age = age + 1;
-    print("{s} is {} years old!", .{ person, age });
+    print("{s} is {d} years old!", .{ person, age });
 
     // Arrays
     util.title("Arrays");
     const skills = [_]u8{ 'Z', 'i', 'g' }; // array of infered size chars (integer under the hood)
     const commits = [_]u8{ 1, 2, 3 };
     const degrees = [_][]const u8{ "High School", "Grad School" }; // inferrd sized array of unknown sized string
-    print("Skills: {s}\nCommits: {d}\nDegrees: {s}", .{ skills, commits, degrees });
+    print("Skills: {s}\nCommits: {any}\nDegrees: {any}", .{ skills, commits, degrees });
 
     // If Expressions
     util.title("If Expressions");
@@ -31,17 +34,19 @@ pub fn main() void {
 
     // While loops
     util.title("While Loops");
+    print("this is not my name", .{});
+
     const year_of_birth: u16 = 2001;
     var years_old: u8 = 0;
-    const current_ts = std.time.timestamp(); // Seconds from relative to 1970-01-01
-    const year = @divFloor(current_ts, 365 * std.time.s_per_day) + 1970;
+    const current_ts = std.Io.Timestamp.now(init.io, .awake); // Seconds from relative to 1970-01-01
+    const year = @divFloor(current_ts.nanoseconds, 365 * std.time.s_per_day) + 1970;
 
     while (years_old <= 50) {
         const current_year = year_of_birth + years_old;
         const is_future = current_year > year;
         const verb = if (is_future) "will be" else "was";
 
-        if (is_future) print("In {} {s} {s} {} years old\n", .{ current_year, person, verb, years_old });
+        if (is_future) print("In {d} {s} {s} {d} years old\n", .{ current_year, person, verb, years_old });
         years_old = years_old + 1;
     }
 
@@ -54,5 +59,9 @@ pub fn main() void {
     for (degrees, 0..) |degree, index| {
         print("{s}", .{degree});
         if (index == degrees.len - 1) print("\n", .{}) else print(", ", .{});
+    }
+
+    for (0..10) |i| {
+        print("{d}\n", .{i});
     }
 }
